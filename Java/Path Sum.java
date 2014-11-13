3d66808710083c753438b7501966b19a91772082
Yeti Sno <yeti@yetiz.org>
2014-11-14 00:12:34 +0800

    correct answer

--- Java/Path Sum.java
+++ Java/Path Sum.java
@@ -11,24 +11,15 @@ public class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null)
         return false;
-        if (root.left != null){
-            return hasPathSum(root.left, sum - root.val);
+        if (root.left != null && hasPathSum(root.left, sum - root.val)){
+            return true;
         }
-        if (root.right != null){
-            return hasPathSum(root.right, sum - root.val);
+        if (root.right != null && hasPathSum(root.right, sum - root.val)){
+            return true;
         }
         if (root.left == null && root.right == null){
-            
+            return (sum - root.val) == 0;
         }
         return false;
     }
 }
-
-/*
-
-       1
-   -2    -3
-  1  3  -2
--1
-
-*/
